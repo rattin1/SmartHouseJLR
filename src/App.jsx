8e9d68bd7c6d2aa2a36f041f5cedaa4908de847b
Luiz G. <luiz.mwlo76@gmail.com>
2025-08-13 08:07:13 -0300

    iniciando o design

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
   const ledStatus = "desligada"; // This can be dynamically set based on your application logic
   return (
     <>
-      <div className="container text-light bg-dark p-3 rounded-3 shadow border border-danger d-flex gap-3 flex-column">
+      <div className="container text-dark  d-flex gap-3 flex-column">
         <div className="d-flex flex-row justify-content-between">
           <h1>Garagem</h1>
-          <div className="d-flex flex-column">
-            <span className="text-info fw-bold"> LUZ DA GARAGEM</span>
+          <div className="d-flex flex-column bg-dark bg-opacity-75 p-3 rounded-5">
+            <span className="text-info fw-bold">LUZ DA GARAGEM</span>
             <span className="small text-opacity-50"> Status atual: <p className={`${ ledStatus == "ligada" ? "text-success" : "text-danger"  }`}>{ledStatus}</p></span>
           </div>
 
