a1ad2a5ab75437769bddc10f5fbded0b042a18c5
Joaquim <jocaq124@gmail.com>
2025-08-13 10:21:46 -0300

    Feito gráfico para receber informações da temperatura e umidade atual

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.js";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import Tuchart from './components/TUchart/Tuchart';
 
 function App() {
   
 
   return (
-   
+   <div>
+    <Tuchart/>
+   </div>
   )
 
 }
